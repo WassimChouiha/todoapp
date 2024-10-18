c7459cdc3f00b0c308a7795562b525c3d67c6b1c
wassim chouiha <chouihawassim@gmail.com>
2024-10-18 22:46:35 +0100

    fixed routing

--- src/app/(auth)/dashboard/NavBar.tsx
+++ src/app/(auth)/dashboard/NavBar.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { useAuth } from "@/providers/AuthProvider";
+import Link from "next/link";
 import React from "react";
 
 const Navbar: React.FC = () => {
@@ -11,12 +12,12 @@ const Navbar: React.FC = () => {
         <div className=" bg-gray-50 rounded-lg">
           <div className=" p-4">
             <nav className="flex justify-between">              
-              <a
+              <Link
                 className="px-4 py-2 mt-2 text-xl text-white bg-lime-500 font-semibold rounded-lg  hover:bg-lime-600 focus:bg-lime-600 focus:outline-none focus:shadow-outline"
                 href="/dashboard/my-task"
               >
                 MyTask
-              </a>
+              </Link>
               <Button                
                 className="px-4 py-2 mt-2 text-xl bg-red-500 font-semibold rounded-lg  hover:bg-red-600 focus:bg-red-600 focus:outline-none focus:shadow-outline"
                 onClick={() => {
--- src/app/(no-auth)/NavBar.tsx
+++ src/app/(no-auth)/NavBar.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Link from "next/link";
 import React from "react";
 
 const Navbar: React.FC = () => {
@@ -8,19 +9,19 @@ const Navbar: React.FC = () => {
         <div className="bg-gray-50 rounded-lg">
           <div className="p-4">
             <nav className="flex justify-between">
-              <a
+              <Link
                 className="px-4 py-2 mt-2 text-3xl flex items-center gap-2 font-semibold rounded-lg text-lime-600 bg-transparent focus:outline-none focus:shadow-outline"
                 href="/"
               >
                 <img src="./Vector.svg" />
                 Lists
-              </a>
-              <a
+              </Link>
+              <Link
                 className="px-4 py-2 mt-2 text-xl items-center flex text-white bg-lime-500 font-semibold rounded-lg  hover:bg-lime-600 focus:bg-lime-600 focus:outline-none focus:shadow-outline"
                 href="/login"
               >
                 Login
-              </a>
+              </Link>
             </nav>
           </div>
         </div>
