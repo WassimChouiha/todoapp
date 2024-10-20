5476c58f8f0b33836e0fb6584a412a2fdb67f5c8
WassimChouiha <chouihawassim@gmail.com>
2024-10-20 14:21:48 +0100

    design changes

--- src/app/(no-auth)/NavBar.tsx
+++ src/app/(no-auth)/NavBar.tsx
@@ -5,28 +5,22 @@ import Image from "next/image";
 
 const Navbar: React.FC = () => {
   return (
-    <div className="flex px-2">
-      <div className="w-[1200px] ml-[20%] border-b-2 border-transparent">
-        <div className="bg-gray-50 rounded-lg">
-          <div className="p-4">
-            <nav className="flex justify-between">
-              <Link
-                className="px-4 py-2 mt-2 text-3xl flex items-center gap-2 font-semibold rounded-lg text-lime-600 bg-transparent focus:outline-none focus:shadow-outline"
-                href="/"
-              >
-                <Image width={50} height={50} alt="logo"  src="./Vector.svg" />
-                Lists
-              </Link>
-              <Link
-                className="px-4 py-2 mt-2 text-xl items-center flex text-white bg-lime-500 font-semibold rounded-lg  hover:bg-lime-600 focus:bg-lime-600 focus:outline-none focus:shadow-outline"
-                href="/login"
-              >
-                Login
-              </Link>
-            </nav>
-          </div>
-        </div>
-      </div>
+    <div className="w-4/6 mx-auto border-b-2 border-transparent bg-gray-100 rounded-lg px-4 py-2">
+      <nav className="flex justify-between">
+        <Link
+          className="px-4 py-2 mt-2 text-3xl flex items-center gap-2 font-semibold rounded-lg text-lime-600 bg-transparent focus:outline-none focus:shadow-outline"
+          href="/"
+        >
+          <Image width={50} height={50} alt="logo" src="./Vector.svg" />
+          Lists
+        </Link>
+        <Link
+          className="px-4 py-2 mt-2 text-xl items-center flex text-white bg-lime-500 font-semibold rounded-lg  hover:bg-lime-600 focus:bg-lime-600 focus:outline-none focus:shadow-outline"
+          href="/login"
+        >
+          Login
+        </Link>
+      </nav>
     </div>
   );
 };
--- src/app/(no-auth)/login/page.tsx
+++ src/app/(no-auth)/login/page.tsx
@@ -5,7 +5,7 @@ import { useAuth } from "@/providers/AuthProvider";
 import Link from "next/link";
 import { useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
-
+import Image from "next/image";
 interface IFormInput {
   username: string;
   password: string;
@@ -27,36 +27,42 @@ export default function Login() {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div className="w-full p-8 flex flex-col items-center">
-        <h1 className="text-lime-500 font-bold text-4xl">Login</h1>
-      {error && <p>username or password incorrect</p>}
+    <div className="flex h-screen items-center">
+      <form
+        className="w-full flex flex-col items-center"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <div className="w-[33%] flex flex-col items-center bg-gray-100 rounded-lg py-4">
+        <h1 className="text-lime-500 font-bold text-4xl mt-2">Login</h1>
+        {error && <p>username or password incorrect</p>}
         <Input
           {...register("username", { required: "username is required" })}
           placeholder="UserName"
-          className="w-2/12 mt-8 mb-2"
+          className="w-8/12 mt-8 mb-2"
         />
-
         {errors.username && <p>{errors.username.message}</p>}
         <Input
           {...register("password", { required: "password is required" })}
           type="password"
           placeholder="Password"
-          className="w-2/12 m-2"
+          className="w-8/12 m-2"
         />
         {errors.password && <p>{errors.password.message}</p>}
-        <div>
+        <div className="w-full flex justify-around items-center p-2">
           <Button className="bg-lime-500 hover:bg-lime-600" type="submit">
             Log in
           </Button>
           <Link
-            className="ml-24 text-lime-500 hover:text-lime-600"
+            className=" text-lime-500 hover:text-lime-600"
             href="/register"
           >
-          dont have an account yet?
+            Register
           </Link>
         </div>
-      </div>
-    </form>
+        </div>
+      </form>
+
+      <Image width={650} height={250} src="./Laptop.svg" alt="laptop" />
+    </div>
   );
 }
